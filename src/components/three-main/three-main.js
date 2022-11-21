7f93d1ad5d955469ec75644cbaf060ca47b6f84d
Jerromy <jerromy_lee@nox.com.tw>
2022-11-22 04:59:02 +0800

    Complete Basic AR

--- src/components/three-main/three-main.js
+++ src/components/three-main/three-main.js
@@ -5,29 +5,6 @@ import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRe
 
 const THREE = window.MINDAR.IMAGE.THREE
 
-function Element( id, x, y, z, ry ) {
-
-    const div = document.createElement( 'div' );
-    div.style.width = '480px';
-    div.style.height = '360px';
-    div.style.backgroundColor = '#000';
-
-    const iframe = document.createElement( 'iframe' );
-    iframe.style.width = '480px';
-    iframe.style.height = '360px';
-    iframe.style.border = '0px';
-    iframe.src = [ 'https://www.youtube.com/embed/', id, '?rel=0' ].join( '' );
-    div.appendChild( iframe );
-
-    const object = new CSS3DObject( div );
-    object.position.set( x, y, z );
-    object.rotation.y = ry;
-
-    return object;
-
-}
-
-
 
 class ThreeMain {
 
@@ -85,18 +62,36 @@ class ThreeMain {
         
         });
 
-        this.scene = this.mindarThree.scene
+        this.scene = this.mindarThree.cssScene
         this.camera = this.mindarThree.camera
-        this.renderer = this.mindarThree.renderer
-        console.log( this.mindarThree.renderer )
-
-
-        this.anchor = this.mindarThree.addAnchor( 0 )
-        this.geometry = new THREE.PlaneGeometry( 1, 0.55 )
-        this.material = new THREE.MeshBasicMaterial( { color: 0x00ffff, transparent: true, opacity: 0.5 } )
-        this.plane = new THREE.Mesh( this.geometry, this.material )
+        this.renderer = this.mindarThree.cssRenderer
         
-        this.anchor.group.add( this.plane )
+        
+        // console.log( this.mindarThree.renderer )
+
+
+        this.anchor = this.mindarThree.addCSSAnchor( 0 )
+
+        this.cardDiv = document.createElement( 'div' )
+		this.cardDiv.className = 'card'
+
+        this.id = "SJOz3qjfQXU"
+
+        this.cardObj = new CSS3DObject( this.cardDiv )
+        this.anchor.group.add( this.cardObj )
+        const iframe = document.createElement( 'iframe' )
+		iframe.style.width = '1000px'
+		iframe.style.height = '750px'
+		iframe.style.border = '0px'
+		iframe.src = [ 'https://www.youtube.com/embed/', this.id, '?rel=0' ].join( '' )
+		this.cardDiv.appendChild( iframe )
+
+
+        // this.geometry = new THREE.PlaneGeometry( 1, 0.55 )
+        // this.material = new THREE.MeshBasicMaterial( { color: 0x00ffff, transparent: true, opacity: 0.5 } )
+        // this.plane = new THREE.Mesh( this.geometry, this.material )
+        
+        // this.anchor.group.add( this.plane )
 
         
         this.initEvent()
